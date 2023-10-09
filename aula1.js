de578f96751edcb6fe2a7bfd9f4b4af22503da5c
Edir Dumaszak <ecaramba@gmail.com>
2023-10-09 17:57:53 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,98 @@
+//Aula de JS
+
+// JS -> ECMAScript 6 2015
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+// https://jstherightway.org/pt-br/
+// https://nodejs.org/en/download
+// https://eloquentjavascript.net/
+
+// interpretada 
+// fracamente tipada
+
+var nome = "edir"; // string
+var idade = 13; // number int
+var peso = 45.9; // number float
+var acordado = true; // boolean
+
+const n1 = 4;
+console.log(n1);
+let n1 = 3;
+var n2 = 5;
+var n3 = 4;
+
+function soma(n1, n2){
+	let n3 = 3;
+	return n1 + n2 + n3;
+	
+	if (){
+		let a = 1;
+	}
+	
+	var a = 3;
+	
+	if (a == 2) {
+		var a = 2;
+	}
+	
+	function sub(n1,n2){
+		return n1 - n3
+		
+	}
+}
+
+if (){
+	let n1 = 2
+}
+
+soma(2,3)
+const multi = function(variavel) {
+	return variavel()
+}
+
+// callback
+multi(soma);
+
+const abc = (function(){
+	console.log("abc")
+	multi()
+})abc()
+
+const mult = (arg) => { return "abc"}
+const mult = (arg) =>  "abc" + arg;
+
+
+let nome = new String("Edir");
+
+function Pessoa(){
+	
+}
+
+let edir = new Pessoa();
+
+class Pessoa {
+	
+	constructor(n1) {
+		this.n1 = n1;
+	}
+	
+	abc () {
+		mult()
+	}
+	
+}
+
+let cidades = ["curitiba", "colombo", "Pinhais"];
+
+cidades.push("Araucaria");
+
+// JSON
+
+
+
+
+
+
+
+
+
+
